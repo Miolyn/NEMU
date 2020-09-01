ce1d868e4ee5d304f7309a6844d9a2986315868e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-01 06:12:09 +0000

    >  compile NEMU 3018216166 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:12:09 up  4:27,  1 user,  load average: 0.11, 0.15, 0.20 cd555ace2a3b85fa5ce11416f3349696d2ff73a3

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,16 +15,18 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-	struct {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-	} gpr[8];
+	union {
+        union {
+		    uint32_t _32;
+		    uint16_t _16;
+		    uint8_t _8[2];
+	    } gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+        struct {
+	        uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        };
+    };
 	swaddr_t eip;
 
 } CPU_state;
