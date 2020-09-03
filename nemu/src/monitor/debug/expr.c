6aced9f4c4ca003f542aa5740c30d56568bed2ff
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-03 04:43:45 +0000

    >  compile NEMU 3018216166 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:43:45 up 1 day, 19:08, 20 users,  load average: 1.58, 1.53, 1.43 fdd2e1735733576f1374799e3f741879c640c2b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -283,7 +283,7 @@ int find_dominant_operator(bool *success, uint32_t p, uint32_t q){
     int i;
     // find deref
     bool flag = false;
-    if(tokens[p].type == DEREF || tokens[p].type == NEG){
+    if((tokens[p].type == DEREF || tokens[p].type == NEG || tokens[p].type == LNOT) && p + 1 == q){
         op = p;
         flag = true;
         return op;
