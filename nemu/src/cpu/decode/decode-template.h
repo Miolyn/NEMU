2d2de8447176157a56d154b5238adcaac04f30e2
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 20:14:28 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:14:28 up  2:45,  2 users,  load average: 0.10, 0.10, 0.09 e5d420ad6fc249e8438430c845838cd19d982ac2

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -34,7 +34,7 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	 */
 	op_src->simm = instr_fetch(eip, DATA_BYTE);
 
-	panic("please implement me");
+	// panic("please implement me");
 
 	op_src->val = op_src->simm;
 
