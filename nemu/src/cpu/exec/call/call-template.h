bed9a88306ac4e481cfdcaec0776db6b3b2cac57
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 19:56:59 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:56:59 up 15:02,  2 users,  load average: 0.04, 0.02, 0.00 5d70c0c9fd8136355d88129954d0ca519070ffc

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -7,26 +7,29 @@
 
 // }
 
-#if DATA_BYTE == 2
-make_helper(call_rel_l){
-    printf("start call_rel_l at %x, %x\n", eip, instr_fetch(eip + 1, 4));
-    // eip += opcode len
-    eip += 1;
-    int len = concat(decode_i_, SUFFIX)(eip);
-    printf("len:%d,src:%d,call_op-push_addr:%x\n", len, op_src->imm, eip + len);
-    PUSH_STACK((eip + len) & 0xFFFF);
-    cpu.eip = (eip + op_src->imm) & 0xFFFF;
-    return len;
-}
-#endif
+// #if DATA_BYTE == 2
+// make_helper(call_rel_l){
+//     printf("start call_rel_l at %x, %x\n", eip, instr_fetch(eip + 1, 4));
+//     // eip += opcode len
+//     eip += 1;
+//     int len = concat(decode_i_, SUFFIX)(eip);
+//     printf("len:%d,src:%d,call_op-push_addr:%x\n", len, op_src->imm, eip + len);
+//     PUSH_STACK((eip + len) & 0xFFFF);
+//     cpu.eip = (eip + op_src->imm) & 0xFFFF;
+//     return len;
+// }
+// #endif
 
-#if DATA_BYTE == 4
-make_helper(call_rel_w){
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_helper(concat(call_rel_, SUFFIX)){
     printf("start call_rel_%d at %x, %x\n", DATA_BYTE, eip, instr_fetch(eip + 1, 4));
     eip += 1;
     int len = concat(decode_i_, SUFFIX)(eip);
-    
+    printf("len:%d,src:%d,call_op-push_addr:%x\n", len, op_src->imm, eip + len);
     if(ops_decoded.is_operand_size_16){
+        PUSH_STACK((eip + len) & 0xFFFF);
+        cpu.eip = (eip + op_src->imm) & 0xFFFF;
+    } else{
         PUSH_STACK(eip + len);
         cpu.eip = eip + op_src->imm;
     }
