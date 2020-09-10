4e3b3daed845d98ffab105157f4f4cc1399df497
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 18:59:31 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:59:31 up 14:05,  2 users,  load average: 0.01, 0.01, 0.00 a78d3470faa4d9c9b83470f0ca0ad40d7d41b1b8

--- nemu/src/cpu/exec/ret/ret-template.h
+++ nemu/src/cpu/exec/ret/ret-template.h
@@ -4,17 +4,19 @@
 
 #if DATA_BYTE == 2
 make_helper(concat(ret_rel_, SUFFIX)){
+    printf("ret%d read %x", DATA_BYTE, swaddr_read(REG(R_SP), DATA_BYTE) & 0xFFFF);
     cpu.eip = swaddr_read(REG(R_SP), DATA_BYTE) & 0xFFFF;
     REG(R_SP) = REG(R_SP) + DATA_BYTE;
-    return -1;
+    return 1;
 }
 #endif
 
 #if DATA_BYTE == 4
 make_helper(concat(ret_rel_, SUFFIX)){
+    printf("ret%d read %x", DATA_BYTE, swaddr_read(REG(R_SP), DATA_BYTE) & 0xFFFF);
     cpu.eip = swaddr_read(REG(R_SP), DATA_BYTE);
     REG(R_SP) = REG(R_SP) + DATA_BYTE;
-    return -1;
+    return 1;
 }
 #endif
 
