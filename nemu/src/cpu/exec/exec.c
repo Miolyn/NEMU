aece7da5264f140c408c88dcea9e9369ca845942
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 18:31:56 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:31:56 up 13:37,  2 users,  load average: 0.00, 0.00, 0.00 593c758be30e31cda2126b4b3b8b0c10400f3f35

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -131,7 +131,7 @@ helper_fun opcode_table [256] = {
 /* 0x78 */	inv, inv, inv, inv,
 /* 0x7c */	inv, inv, inv, inv,
 /* 0x80 */	group1_b, group1_v, inv, group1_sx_v, 
-/* 0x84 */	test_r2rm_v, inv, inv, inv,
+/* 0x84 */	test_r2rm_b, test_r2rm_v, inv, inv,
 /* 0x88 */	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
 /* 0x8c */	inv, inv, inv, pop_m_v,
 /* 0x90 */	inv, inv, inv, inv,
