4bd1c84d8f419e6ceee7b6fedcd610d7fd74b018
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 17:57:29 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:57:29 up 13:03,  2 users,  load average: 0.00, 0.00, 0.03 fbc3623473d6b87a953d1624214b4dbef4f29af1

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -12,8 +12,8 @@ make_helper(call_rel_l){
     printf("start call_rel_l\n");
     int len = decode_i_l(eip);
     PUSH_STACK(eip + len);
-    cpu.eip = eip + len + op_src->imm;
-    return -1;
+    cpu.eip = eip + op_src->imm;
+    return len;
 }
 #endif
 
@@ -22,9 +22,9 @@ make_helper(call_rel_w){
     printf("start call_rel_w\n");
     int len = decode_i_w(eip);
     PUSH_STACK(eip + len);
-    cpu.eip = (eip + len + op_src->imm) & 0xFFFF;
+    cpu.eip = (eip + op_src->imm) & 0xFFFF;
     // 0x66 prefix return length=1
-    return -1;
+    return len;
 }
 #endif
 
