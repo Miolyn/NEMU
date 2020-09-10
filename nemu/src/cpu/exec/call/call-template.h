0c40e5c1b466cc5bfcb916c66774baa58ac10bf8
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-09 18:04:57 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-45-generic #49~18.04.2-Ubuntu SMP Wed Aug 26 16:29:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:04:57 up 13:10,  2 users,  load average: 0.08, 0.02, 0.01 2f51f79936af5a4a20919a2f5d32d9a281fb8ec1

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -9,23 +9,28 @@
 
 #if DATA_BYTE == 2
 make_helper(call_rel_l){
-    printf("start call_rel_l at %x, %x\n", eip, instr_fetch(eip, 4));
+    printf("start call_rel_l at %x, %x\n", eip, instr_fetch(eip + 1, 4));
+    // eip += opcode len
+    eip += 1;
     int len = decode_i_l(eip);
     printf("len%d,src%d\n", len, op_src->imm);
     PUSH_STACK(eip + len);
     cpu.eip = eip + op_src->imm;
-    return len;
+    // 1 means the len of the opcode
+    return len + 1;
 }
 #endif
 
 #if DATA_BYTE == 4
 make_helper(call_rel_w){
     printf("start call_rel_w\n");
+    eip += 1;
     int len = decode_i_w(eip);
     PUSH_STACK(eip + len);
     cpu.eip = (eip + op_src->imm) & 0xFFFF;
     // 0x66 prefix return length=1
-    return len;
+    // 1 means the len of the opcode
+    return len + 1;
 }
 #endif
 
