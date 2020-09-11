cfa2016ed3220fa053614b64e83433b5f9b53a19
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-10 23:36:32 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:36:32 up  1:19,  3 users,  load average: 0.21, 0.14, 0.10 d9a1c4e6a6712e74ffff34a081c6d1502597bf5

--- nemu/src/cpu/exec/data-mov/movs-template.h
+++ nemu/src/cpu/exec/data-mov/movs-template.h
@@ -2,4 +2,13 @@
 
 #define instr movs
 
+#if DATA_BYTE != 1
+make_helper(concat(movs_m_, SUFFIX)){
+    MEM_W(MEM_R(REG(R_EDI)), MEM_R(REG(R_ESI)));
+    int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
+    REG(R_ESI) = REG(R_ESI) + incDec;
+    REG(R_EDI) = REG(R_EDI) + incDec;
+}
+#endif
+
 #include "cpu/exec/template-end.h"
