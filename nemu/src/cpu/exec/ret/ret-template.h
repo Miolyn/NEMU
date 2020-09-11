b7d75c0acd4755fe00a69efd6f2151ee191b5ec8
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-11 05:29:42 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:29:42 up  5:48,  2 users,  load average: 0.24, 0.16, 0.10 5288da945df299d56f0def71ea124c0f3b4bd4b7

--- nemu/src/cpu/exec/ret/ret-template.h
+++ nemu/src/cpu/exec/ret/ret-template.h
@@ -21,9 +21,9 @@ make_helper(concat(ret_rel_, SUFFIX)){
 make_helper(concat(ret_i_, SUFFIX)){
     printf("ret at rsp%x\n", reg_l(R_ESP));
     if (ops_decoded.is_operand_size_16){
-        cpu.eip = swaddr_read(REG(R_ESP), 4) & 0xFFFF;
+        cpu.eip = swaddr_read(reg_l(R_ESP), 4) & 0xFFFF;
     } else{
-        cpu.eip = swaddr_read(REG(R_SP), 4);
+        cpu.eip = swaddr_read(reg_l(R_SP), 4);
     }
     
     REG(R_ESP) = REG(R_ESP) + 4;
