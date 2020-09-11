98045807963d7c4d4226dd90be293ba04cacd20f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-11 02:32:36 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:32:36 up  4:15,  2 users,  load average: 0.10, 0.10, 0.09 2dba8c33380a0aa3ad2291423f4267f8e0fb1adf

--- nemu/src/cpu/exec/lea/lea-template.h
+++ nemu/src/cpu/exec/lea/lea-template.h
@@ -4,13 +4,11 @@
 
 #if DATA_BYTE != 1
 static void do_execute(){
-    printf("src:%x,dest:%x\n", op_src->addr, op_dest->reg);
-    assert(0);
-    // if(ops_decoded.is_operand_size_16){
-    //     reg_w(op_dest->reg) = op_src->addr & 0xFFFF;
-    // } else{
-    //     reg_l(op_src->reg) = op_src->addr;
-    // }
+    if(ops_decoded.is_operand_size_16){
+        reg_w(op_dest->reg) = op_src->addr & 0xFFFF;
+    } else{
+        reg_l(op_dest->reg) = op_src->addr;
+    }
 }
 make_instr_helper(rm2r)
 #endif
