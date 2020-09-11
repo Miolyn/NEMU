2830ac2f7e105ee21dae0cb6dfe06aa834821d00
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-11 01:17:39 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:17:39 up  3:00,  2 users,  load average: 0.28, 0.13, 0.10 a3139572aa96ea6869273ba81449bb51497d5ee

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -36,9 +36,9 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	op_src->simm = instr_fetch(eip, DATA_BYTE);
 	printf("res %x\n", (op_src->simm >> (DATA_BYTE * 8 - 1)));
 	if ((op_src->simm >> (DATA_BYTE * 8 - 1)) & 1){
-		op_src->simm |= 0xFFFFFFFF & (1 << (DATA_BYTE * 8 - 1));
+		op_src->simm |= 0xFFFFFFFF ^ (1 << (DATA_BYTE * 8 - 1));
 		printf("src %x\n", op_src->simm);
-		printf("neg : %x\n", 0xFFFFFFFF & (1 << (DATA_BYTE * 8 - 1)));
+		printf("neg : %x\n", 0xFFFFFFFF ^ (1 << (DATA_BYTE * 8 - 1)));
 	}
 	// panic("please implement me");
 
