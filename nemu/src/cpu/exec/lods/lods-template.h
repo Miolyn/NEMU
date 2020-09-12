6d1c541c3d31a8c019bd054639eed7586251fed5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-11 17:56:50 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:56:50 up  7:52,  2 users,  load average: 0.13, 0.10, 0.09 ae20f212848ed508efc2d2ea72468ddada0d6af1

--- nemu/src/cpu/exec/lods/lods-template.h
+++ nemu/src/cpu/exec/lods/lods-template.h
@@ -13,6 +13,7 @@ make_helper(concat(lods_m_, SUFFIX)){
 
 #if DATA_BYTE == 1
 make_helper(concat(lods_m_, SUFFIX)){
+    printf("lods\n");
     printf("esi:%x,edi:%x\n", MEM_R(reg_w(R_ESI)), MEM_R(reg_w(R_EDI)));
     MEM_W(reg_w(R_ESI), REG(R_EAX));
     int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
