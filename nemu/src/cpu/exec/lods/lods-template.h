3760a67c929a5d968b3eb563f87ceedd89047a6d
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-11 18:08:08 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:08:08 up  8:03,  2 users,  load average: 0.14, 0.10, 0.09 1cd24edece226d529d9055f3817eefda561c39c9

--- nemu/src/cpu/exec/lods/lods-template.h
+++ nemu/src/cpu/exec/lods/lods-template.h
@@ -17,7 +17,9 @@ make_helper(concat(lods_m_, SUFFIX)){
     printf("esi:%x,edi:%x\n", MEM_R(reg_w(R_ESI)), MEM_R(reg_w(R_EDI)));
     MEM_W(reg_w(R_ESI), REG(R_EAX));
     int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
+    printf("esi before %x\n", reg_w(R_ESI));
     reg_w(R_ESI) = reg_w(R_ESI) + incDec;
+    printf("esi after %x\n", reg_w(R_ESI));
     return 1;
 }
 #endif
