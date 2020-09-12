1696a66457ec98dfcda6e6dff9f897beeed44004
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-12 00:25:02 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:25:02 up 13:47,  2 users,  load average: 0.15, 0.18, 0.18 d7a412cb3ec52d38916c19e895c667c9a2a3e208

--- nemu/src/cpu/exec/ret/ret-template.h
+++ nemu/src/cpu/exec/ret/ret-template.h
@@ -6,14 +6,14 @@
 make_helper(concat(ret_rel_, SUFFIX)){
     printf("``````````````````````````````````````````````````````````````````````````````\n");
     printf("start to deal with ret%d\n", DATA_BYTE);
-    printf("ret%d read %x\n", DATA_BYTE, swaddr_read(REG(R_SP), DATA_BYTE) );
+    printf("ret%d read %x\n", DATA_BYTE, swaddr_read(reg_l(R_SP), 4) );
     if (ops_decoded.is_operand_size_16){
-        cpu.eip = swaddr_read(REG(R_ESP), DATA_BYTE) & 0xFFFF;
+        cpu.eip = swaddr_read(reg_l(R_ESP), 4) & 0xFFFF;
     } else{
-        cpu.eip = swaddr_read(REG(R_SP), DATA_BYTE);
+        cpu.eip = swaddr_read(reg_l(R_SP), 4);
     }
     
-    REG(R_ESP) = REG(R_ESP) + 4;
+    reg_l(R_ESP) = reg_l(R_ESP) + 4;
     print_asm(str(instr) str(SUFFIX));
     // control the len 
     // read one opcode before deal
@@ -28,7 +28,7 @@ make_helper(concat(ret_i_, SUFFIX)){
         cpu.eip = swaddr_read(reg_l(R_SP), 4);
     }
     
-    REG(R_ESP) = REG(R_ESP) + 4;
+    reg_l(R_ESP) = reg_l(R_ESP) + 4;
     decode_i_w(eip + 1);
     printf("eip:%x,val:%x\n", cpu.eip, op_src->val);
     reg_l(R_ESP) = reg_l(R_ESP) + op_src->val;
