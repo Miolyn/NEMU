aa30590d7b7e2b79522f1cc5fda90280bab3ff70
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-12 18:29:27 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:29:27 up 12 min,  2 users,  load average: 0.12, 0.42, 0.45 a8c47d5e24a321d1900f3a5ba4047444a520a63e

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -7,12 +7,11 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	// panic("please implement me");
 	reset_eflags(CF);
 	reset_eflags(OF);
-	sign_flag(result);
-	zero_flag(result);
-	parity_flag(result);
+	szp(result, DATA_BYTE);
+
 	print_asm_template2();
 }
 
