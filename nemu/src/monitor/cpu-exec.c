513643eb6cb04e0ff65fa5911d8f4eccd8640d83
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-13 03:21:56 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:21:56 up  3:58,  3 users,  load average: 0.08, 0.09, 0.09 2a310a3fc66bc3134e7ad0672b0b8de17f1ac88

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -79,10 +79,12 @@ void cpu_exec(volatile uint32_t n) {
 		printf("reg_%s:0x%x ;\n", regsl[j], reg_l(j));
 		j = R_ESP;
 		if (reg_l(j) < (1 << (10 + 10 + 3 + (27 - 10 - 10 - 3)))){
-			printf("reg %s:0x%x ,mem:l:0x%x,w0x%x,b0x%x ;\n", regsl[j], reg_l(j), swaddr_read(reg_l(j), 4), swaddr_read(reg_l(j), 2), swaddr_read(reg_l(j), 1));
+			printf("reg %s:0x%x ,mem:l:0x%x,w:0x%x,b:0x%x ;\n", regsl[j], reg_l(j), swaddr_read(reg_l(j), 4), swaddr_read(reg_l(j), 2), swaddr_read(reg_l(j), 1));
 		} else{
 			printf("reg_%s:0x%x ;\n", regsl[j], reg_l(j));
 		}
+		printf("CF:%d; PF:%d; AF:%d; ZF:%d; SF:%d; OF:%d; \n", reg_eflags(CF),
+			reg_eflags(PF), reg_eflags(AF), reg_eflags(ZF), reg_eflags(SF), reg_eflags(OF));
 		printf("----------------------------------------------------------------------------\n");
 		cpu.eip += instr_len;
 
