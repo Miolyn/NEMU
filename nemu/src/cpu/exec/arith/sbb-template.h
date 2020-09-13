6d2b13a3f3b18a56be799a32b22f9611eb5d24ff
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-13 01:14:14 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:14:14 up  2:45,  2 users,  load average: 0.20, 0.13, 0.10 f4922531359888d51600e3e53cb83328934bde8d

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -3,19 +3,19 @@
 #define instr sbb
 
 static void do_execute(){
-    int res = carry_flag(op_dest->val, -(op_src->val + reg_eflags(CF)));
-    // int res = oszapc(op_dest->val, -(op_src->val + reg_eflags(CF)), DATA_BYTE);
+    // int res = carry_flag(op_dest->val, -(op_src->val + reg_eflags(CF)));
+    int res = sozapc(op_dest->val, -(op_src->val + reg_eflags(CF)), DATA_BYTE);
     // int st = 0xFFFF;
     // if (DATA_BYTE == 1) st = 0xFF;
     // if (DATA_BYTE != 4) res &= st;
     int st = 0xffff;
     if (DATA_BYTE == 1) st = 0xff;
     if (DATA_BYTE != 4) res &= st;
-    parity_flag(res);
-    adjust_flag(op_dest->val, -(op_src->val + reg_eflags(CF)));
-    zero_flag(res);
-    sign_flag(res);
-    overflow_flag(op_dest->val, -(op_src->val + reg_eflags(CF)));
+    // parity_flag(res);
+    // adjust_flag(op_dest->val, -(op_src->val + reg_eflags(CF)));
+    // zero_flag(res);
+    // sign_flag(res);
+    // overflow_flag(op_dest->val, -(op_src->val + reg_eflags(CF)));
     print_temp1();
     concat(write_operand_, SUFFIX)(op_dest, res);
 }
