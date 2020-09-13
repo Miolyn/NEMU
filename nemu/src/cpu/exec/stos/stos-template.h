09e2b90b7d7a0009d21b7851cd8e37ac375658fe
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-12 21:43:56 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:43:56 up 10 min,  2 users,  load average: 0.12, 0.22, 0.18 5a9e7d519f3fccf5f6c44fcd5a262d26ccff862

--- nemu/src/cpu/exec/stos/stos-template.h
+++ nemu/src/cpu/exec/stos/stos-template.h
@@ -2,22 +2,22 @@
 
 #define instr stos
 
-#if DATA_BYTE != 1
-make_helper(concat(stos_m_, SUFFIX)){
-    MEM_W(REG(R_EDI), REG(R_EAX));
-    int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
-    REG(R_EDI) = REG(R_EDI) + incDec;
-    return 1;
-}
-#endif
+// #if DATA_BYTE != 1
+// make_helper(concat(stos_m_, SUFFIX)){
+//     MEM_W(REG(R_EDI), REG(R_EAX));
+//     int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
+//     REG(R_EDI) = REG(R_EDI) + incDec;
+//     return 1;
+// }
+// #endif
 
-#if DATA_BYTE == 1
+// #if DATA_BYTE == 1
 make_helper(concat(stos_m_, SUFFIX)){
-    MEM_W(reg_w(R_EDI), REG(R_EAX));
+    MEM_W(reg_l(R_EDI), REG(R_EAX));
     int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
-    reg_w(R_EDI) = reg_w(R_EDI) + incDec;
+    reg_l(R_EDI) = reg_l(R_EDI) + incDec;
     return 1;
 }
-#endif
+// #endif
 
 #include "cpu/exec/template-end.h"
