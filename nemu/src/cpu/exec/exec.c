ad36cc0b0901fa4a0608d8a57ea972fde7f5772f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-13 19:27:49 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:27:49 up  9:24,  2 users,  load average: 0.11, 0.14, 0.10 dfe4f96998bd44329119c307e407ce9e755ddd6

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -83,7 +83,7 @@ make_group(group4,
 /* 0xff */
 make_group(group5,
 	inc_rm_v, dec_rm_v, call_rm_v, inv, 
-	jmp_rm_v, inv, inv, inv)
+	jmp_rm_v, inv, push_m_v, inv)
 
 make_group(group6,
 	inv, inv, inv, inv, 
