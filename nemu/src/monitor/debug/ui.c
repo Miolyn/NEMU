57120ce6ced6e4e70238156ea3a3adb8e09d93dc
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-14 04:42:32 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:42:32 up 11:50,  2 users,  load average: 0.16, 0.11, 0.09 bb263f3a87da71db79f5079d5c0eeec7458606f8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -146,7 +146,7 @@ static int cmd_pt(char *args){
     int n = atoi(args);
     int i = 0;
     for(i = 0; i < n; i++){
-        printf("+0x%x($esp)=%x", i * 4, swaddr_read(cpu.esp + i * 4, 4));
+        printf("+0x%x($esp)=0x%x\n", i * 4, swaddr_read(cpu.esp + i * 4, 4));
     }
     return 0;
 }
