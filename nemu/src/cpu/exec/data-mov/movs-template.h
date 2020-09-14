6f0c0e5973ebdaa5db26a15e3bc75f16da877afa
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-14 03:23:19 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:23:19 up 10:31,  2 users,  load average: 0.10, 0.10, 0.09 e17c8df0f66a48d2a068bd0a2b143d44d20dd563

--- nemu/src/cpu/exec/data-mov/movs-template.h
+++ nemu/src/cpu/exec/data-mov/movs-template.h
@@ -2,24 +2,13 @@
 
 #define instr movs
 
-#if DATA_BYTE != 1
 make_helper(concat(movs_m_, SUFFIX)){
-    MEM_W(REG(R_EDI), MEM_R(REG(R_ESI)));
+    MEM_W(reg_l(R_EDI), MEM_R(reg_l(R_ESI)));
     int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
-    REG(R_ESI) = REG(R_ESI) + incDec;
-    REG(R_EDI) = REG(R_EDI) + incDec;
-    
+    reg_l(R_ESI) = reg_l(R_ESI) + incDec;
+    reg_l(R_EDI) = reg_l(R_EDI) + incDec;
+    print_asm_template5();
     return 1;
 }
-#endif
 
-#if DATA_BYTE == 1
-make_helper(concat(movs_m_, SUFFIX)){
-    MEM_W(reg_w(R_DI), MEM_R(reg_w(R_SI)));
-    int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
-    reg_w(R_ESI) = reg_w(R_ESI) + incDec;
-    reg_w(R_EDI) = reg_w(R_EDI) + incDec;
-    return 1;
-}
-#endif
 #include "cpu/exec/template-end.h"
--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -101,7 +101,7 @@ static int cmd_x(char *args){
     for (i = 0; i < n; i++){
         uint32_t addrI = uAddr + i * 4;
         uint32_t val = swaddr_read(addrI, 4);
-        printf("addr:0x%x,val:%08x\n", addrI, val);
+        printf("addr:0x%x,val:0x%x\n", addrI, val);
         int j;
         for(j = 0; j < 4; j++){
             val = swaddr_read(addrI + j, 1);
