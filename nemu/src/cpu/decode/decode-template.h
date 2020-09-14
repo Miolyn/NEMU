81513102fa022507bace64847c919bc8080b4ce8
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-14 05:34:26 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:34:26 up 12:42,  2 users,  load average: 0.13, 0.11, 0.09 d6c75dc34d354145ff52f8ae8e774cb839ebc47

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -256,23 +256,27 @@ void_helper(concat(push_stack_, SUFFIX)){
 	// printf("push stack at rsp:%x, data:%x\n", reg_l(R_ESP), src);
 #if DATA_BYTE != 1
 	reg_l(R_ESP) = reg_l(R_ESP) - DATA_BYTE;
+	swaddr_write(reg_l(R_ESP), DATA_BYTE, src);
 #endif
 #if DATA_BYTE == 1
 	reg_l(R_ESP) = reg_l(R_ESP) - 4;
+	swaddr_write(reg_l(R_ESP), 4, src);
 #endif
-	swaddr_write(reg_l(R_ESP), DATA_BYTE, src);
+	
 	// printf("after %x, data:%x \n", reg_l(R_ESP), swaddr_read(reg_l(R_ESP), 4));
 }
 
 void_op_helper(concat(pop_stack_, SUFFIX)) {
 	// printf("pop stack op read %x\n", swaddr_read(REG(R_SP), DATA_BYTE));
+	
 #if DATA_BYTE != 1
-	concat(write_operand_, SUFFIX)(op, swaddr_read(reg_l(R_ESP), DATA_BYTE));
+concat(write_operand_, SUFFIX)(op, swaddr_read(reg_l(R_ESP), DATA_BYTE));
+	reg_l(R_ESP) = reg_l(R_ESP) + DATA_BYTE;
 #endif
-#if DATA_BYTE == 1
+#if DATA_BYTE != 1
+	concat(write_operand_, SUFFIX)(op, swaddr_read(reg_l(R_ESP), 4));
 	reg_l(R_ESP) = reg_l(R_ESP) + 4;
 #endif
-	reg_l(R_ESP) = reg_l(R_ESP) + DATA_BYTE;
 }
 
 
