fd3318e812b617ee1bd3ab4374645d60d25a79a9
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 06:07:49 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:07:49 up 1 day,  1:06,  2 users,  load average: 0.09, 0.12, 0.09 92124f26aa2f81e22a174bf4d7d40439397948

--- nemu/src/cpu/exec/data-mov/movs-template.h
+++ nemu/src/cpu/exec/data-mov/movs-template.h
@@ -3,8 +3,8 @@
 #define instr movs
 
 make_helper(concat(movs_m_, SUFFIX)){
-    // MEM_W(reg_l(R_EDI), MEM_R(reg_l(R_ESI)));
-    swaddr_write(reg_l(R_EDI), 4, swaddr_read(reg_l(R_ESI), 4));
+    MEM_W(reg_l(R_EDI), MEM_R(reg_l(R_ESI)));
+    // swaddr_write(reg_l(R_EDI), 4, swaddr_read(reg_l(R_ESI), 4));
     int incDec = reg_eflags(DF) ? -DATA_BYTE : DATA_BYTE;
     reg_l(R_ESI) = reg_l(R_ESI) + incDec;
     reg_l(R_EDI) = reg_l(R_EDI) + incDec;
