3fa0261a8f14d07c26872a734d8dc6248b8d51b3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 06:55:16 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:55:16 up 1 day,  1:53,  2 users,  load average: 0.16, 0.14, 0.10 631d3e344d62f80268d762049e522b563bdfd90d

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -10,7 +10,7 @@ int main() {
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
-
+	set_bp();
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
