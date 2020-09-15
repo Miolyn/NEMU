9aa978febda5fc497dd78d873ceab3b2a38789c9
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 03:46:56 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:46:56 up 22:45,  2 users,  load average: 0.06, 0.11, 0.09 692c3d539de7c96817c90901f82c615fce5b9c4f

--- nemu/src/cpu/exec/call/call.h
+++ nemu/src/cpu/exec/call/call.h
@@ -1,10 +1,10 @@
 #ifndef __CALL_H__
 #define __CALL_H__
 
-make_helper(call_rel_l);
-make_helper(call_rel_w);
-make_helper(call_rm_l);
-make_helper(call_rm_w);
+// make_helper(call_rel_l);
+// make_helper(call_rel_w);
+// make_helper(call_rm_l);
+// make_helper(call_rm_w);
 
 make_helper(call_rel_v);
 make_helper(call_rm_v);
