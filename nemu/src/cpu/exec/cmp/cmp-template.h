211ae8f29bbfa78fe842ea2a4978b10a0bd43478
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 03:53:42 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:53:42 up 22:52,  2 users,  load average: 0.21, 0.15, 0.10 bbf37c0c57543e1e55fe98c7099e2f7327bf981

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -3,7 +3,12 @@
 #define instr cmp
 
 static void do_execute(){
-    // int res = sozapc(op_dest->val, -op_src->val, DATA_BYTE);
+#if DATA_BYTE == 1
+    op_dest->val &= 0xff; op_src->val &= 0xff;
+#endif
+#if DATA_BYTE == 2
+    op_dest->val &= 0xffff; op_src->val &= 0xff;
+#endif
     printf("l:0x%x,r0x%x\n", op_dest->val, op_src->val);
     uint32_t res = carry_flag(op_dest->val, -op_src->val);
     parity_flag(res);
