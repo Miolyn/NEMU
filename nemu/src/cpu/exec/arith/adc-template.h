e31ba03a3e92649ae6fa6f6af7644b8696c27aa6
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-14 18:40:21 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:40:21 up 15:15,  2 users,  load average: 0.20, 0.11, 0.10 8654aef7730d91f13d783331904cb68b72e8877b

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -4,17 +4,18 @@
 
 static void do_execute(){
     // int res = carry_flag3(op_dest->val, op_src->val);
-    int res = carry_flag(op_dest->val, op_src->val + reg_eflags(CF));
+    int cfv = reg_eflags(CF);
+    int res = carry_flag(op_dest->val, op_src->val + cfv);
     int st = 0xffff;
     if (DATA_BYTE == 1) st = 0xff;
     if (DATA_BYTE != 4) res &= st;
     parity_flag(res);
     // adjust_flag3(op_dest->val, op_src->val);
-    adjust_flag(op_dest->val, op_src->val + reg_eflags(CF));
+    adjust_flag(op_dest->val, op_src->val + cfv);
     zero_flag(res);
     sign_flag(res);
     // overflow_flag3(op_dest->val, op_src->val);
-    overflow_flag(op_dest->val, op_src->val + reg_eflags(CF));
+    overflow_flag(op_dest->val, op_src->val + cfv);
     concat(write_operand_, SUFFIX)(op_dest, res);
 }
 
