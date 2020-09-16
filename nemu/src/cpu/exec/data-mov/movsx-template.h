06b8984aa9232c5088becad8cd5cf36ba7f2578e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 22:01:55 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:01:55 up 1 day,  9:57,  2 users,  load average: 0.08, 0.09, 0.09 41478b2a93ff3d8707c30d15459d726e199828

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,10 +5,11 @@
 #if DATA_BYTE != 1
 make_helper(concat(movsx_rmb2r_, SUFFIX)){
     uint32_t len = decode_rm_b_internal(eip + 1, op_src, op_dest);
-    int32_t res = (int8_t)op_src->val;
-    // if (sign_bit8(res)){
-    //     res |= 0xFFFFFF00;
-    // }
+    // int32_t res = (int8_t)op_src->val;
+    uint32_t res = op_src->val;
+    if (sign_bit8(res)){
+        res |= 0xFFFFFF00;
+    }
     concat(write_operand_, SUFFIX)(op_dest, res);
     print_asm_template2();
     return len + 1;
