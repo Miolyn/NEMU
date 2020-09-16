346990a9976ef0484ccb610a917c89b79474c724
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 03:08:54 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:08:54 up 1 day, 14:34,  2 users,  load average: 0.10, 0.13, 0.10 8649d0b3b7b51bb27e55f13721bbd00ba598fa3d

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -5,27 +5,14 @@
 static void do_execute(){
     concat(write_operand_, SUFFIX)(op_dest, op_dest->val -op_src->val);
 
-	DATA_TYPE result = op_dest->val - op_src->val;
-	int len = (DATA_BYTE << 3) - 1;
-	cpu.CF = op_dest->val < op_src->val;
-	cpu.SF=result >> len;
-    	int s1,s2;
-	s1=op_dest->val>>len;
-	s2=op_src->val>>len;
-    	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
-    	cpu.ZF=!result;
-	result ^= result >>4;
-	result ^= result >>2;
-	result ^= result >>1;
-	cpu.PF=!(result & 1);
 
-    // cf_sub(op_dest->val, op_src->val);
-    // uint32_t res = carry_flag_sub(op_dest->val, op_src->val);
-    // parity_flag(res);
-    // adjust_flag(op_dest->val, -op_src->val);
-    // zero_flag(res);
-    // sign_flag(res);
-    // overflow_flag(op_dest->val, -op_src->val);
+    cf_sub(op_dest->val, op_src->val);
+    DATA_TYPE res = carry_flag_sub(op_dest->val, op_src->val);
+    parity_flag(res);
+    adjust_flag(op_dest->val, -op_src->val);
+    zero_flag(res);
+    sign_flag(res);
+    overflow_flag(op_dest->val, -op_src->val);
     
     print_asm_template2();
 }
