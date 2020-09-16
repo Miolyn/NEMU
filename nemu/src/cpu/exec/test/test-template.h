a578c6d4e7c25e4756fbc1e7ce8093bcc27420d3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 01:41:25 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:41:25 up 1 day, 13:07,  2 users,  load average: 0.09, 0.10, 0.09 44ca554eff99c353061fb159f515d57b7641087

--- nemu/src/cpu/exec/test/test-template.h
+++ nemu/src/cpu/exec/test/test-template.h
@@ -3,7 +3,7 @@
 #define instr test
 
 static void do_execute(){
-    printf("src:0x%x,dest:%x\n", op_src->val, op_dest->val);
+    printf("src:0x%x,dest:%x => 0x%x\n", op_src->val, op_dest->val, op_src->val & op_dest->val);
     
     uint32_t res = op_src->val & op_dest->val;
     cpu.OF = 0;
@@ -14,7 +14,6 @@ static void do_execute(){
     print_asm_template2();
 }
 
-// make_instr_helper(ai)
 make_instr_helper(i2a)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
