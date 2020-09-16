83f8a16ea77222489d7652539af43304bfc600ad
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 21:50:36 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:50:36 up 1 day,  9:45,  2 users,  load average: 0.17, 0.14, 0.10 19f0d874a12083318da5b41fbba0c6ea34a15995

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,14 +3,14 @@
 #define instr sub
 
 static void do_execute(){
-    int res = carry_flag(op_dest->val, -op_src->val);
-    parity_flag(res);
-    adjust_flag(op_dest->val, -op_src->val);
-    zero_flag(res);
-    sign_flag(res);
-    overflow_flag(op_dest->val, -op_src->val);
-    concat(write_operand_, SUFFIX)(op_dest, res);
-    print_temp1();
+    sub_ef;
+    // int res = carry_flag(op_dest->val, -op_src->val);
+    // parity_flag(res);
+    // adjust_flag(op_dest->val, -op_src->val);
+    // zero_flag(res);
+    // sign_flag(res);
+    // overflow_flag(op_dest->val, -op_src->val);
+    concat(write_operand_, SUFFIX)(op_dest, result);
     print_asm_template2();
 }
 
