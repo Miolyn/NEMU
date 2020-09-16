a76d7b546f3067f2dd7f5edf573d3a6a6e5895e5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 17:52:10 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:52:10 up 1 day,  5:47,  2 users,  load average: 0.21, 0.12, 0.10 ecbd7425628257f9998d6c82359e7007a543a981

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,13 +5,10 @@
 #if DATA_BYTE != 1
 make_helper(concat(movsx_rmb2r_, SUFFIX)){
     uint32_t len = decode_rm_b_internal(eip + 1, op_src, op_dest);
-    uint32_t res = op_src->val;
-    if (sign_bit8(res)){
-        res |= 0xFFFFFF00;
-    }
-#if DATA_BYTE == 2
-    res &= 0xffff;
-#endif
+    int32_t res = (int8_t)op_src->val;
+    // if (sign_bit8(res)){
+    //     res |= 0xFFFFFF00;
+    // }
     concat(write_operand_, SUFFIX)(op_dest, res);
     print_asm_template2();
     return len + 1;
--- nemu/src/cpu/exec/data-mov/movsx.c
+++ nemu/src/cpu/exec/data-mov/movsx.c
@@ -16,10 +16,10 @@ make_helper_v(movsx_rmb2r)
 
 make_helper(movsx_rmw2r_l){
     int len = decode_rm_w_internal(eip + 1, op_src, op_dest);
-    uint32_t res = op_src->val;
-    if (sign_bit16(res)){
-        res |= 0xFFFF0000;
-    }
+    int32_t res = (int16_t)op_src->val;
+    // if (sign_bit16(res)){
+    //     res |= 0xFFFF0000;
+    // }
     write_operand_l(op_dest, res);
     print_asm_template2();
     return len + 1;
