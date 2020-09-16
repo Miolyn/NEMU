3b00d8168942467008b3de4487f18145b6ee2fbb
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 03:06:13 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:06:13 up 1 day, 14:32,  2 users,  load average: 0.12, 0.12, 0.10 2ec29ee9cad56fe820f6c3eb6e14a5b78a72c63b

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -5,20 +5,6 @@
 static void do_execute(){
     printf("l:0x%x,r0x%x\n", op_dest->val, op_src->val);
 
-	// DATA_TYPE result = op_dest->val - op_src->val;
-	// int len = (DATA_BYTE << 3) - 1;
-	// cpu.CF = op_dest->val < op_src->val;
-	// cpu.SF=result >> len;
-    // 	int s1,s2;
-	// s1=op_dest->val>>len;
-	// s2=op_src->val>>len;
-    // 	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
-    // 	cpu.ZF=!result;
-	// result ^= result >>4;
-	// result ^= result >>2;
-	// result ^= result >>1;
-	// cpu.PF=!(result & 1);
-
     cf_sub(op_dest->val, op_src->val);
     DATA_TYPE res = op_dest->val - op_src->val;
     parity_flag(res);
