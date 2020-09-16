ea6ccbdd174ca62eac796ac489e9768856a8aeaa
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-15 19:09:25 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:09:25 up 1 day,  7:04,  2 users,  load average: 0.15, 0.13, 0.09 5b6e1d894b027c8a845b5188ffff0e5f83b407

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -4,12 +4,6 @@
 
 static void do_execute(){
     int res = carry_flag(op_dest->val, op_src->val);
-// #if DATA_BYTE == 1
-//     res &= 0xff;
-// #endif
-// #if DATA_BYTE == 2
-//     res &= 0xffff;
-// #endif
     parity_flag(res);
     adjust_flag(op_dest->val, op_src->val);
     zero_flag(res);
