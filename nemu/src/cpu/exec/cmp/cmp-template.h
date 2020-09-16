f14e0920d8a2a1fde5fc2d41af011f68aa74cc1c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 02:21:49 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:21:49 up 1 day, 13:47,  2 users,  load average: 0.17, 0.11, 0.09 d22db8306b3513833a346b6dfc546aca3d225cae

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -4,20 +4,35 @@
 
 static void do_execute(){
     printf("l:0x%x,r0x%x\n", op_dest->val, op_src->val);
-    cpu.tmp = true;
-    cf_sub(op_dest->val, op_src->val);
-    DATA_TYPE res = op_dest->val - op_src->val;
-    res ^= res >>4;
-	res ^= res >>2;
-	res ^= res >>1;
-	cpu.PF=!(res & 1);
-    // parity_flag(res);
-    adjust_flag(op_dest->val, -op_src->val);
-    cpu.ZF = !res;
-    zero_flag(res);
-    cpu.SF = sign_bit32(res);
-    // sign_flag(res);
-    overflow_flag(op_dest->val, -op_src->val);
+
+	DATA_TYPE result = op_dest->val - op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = op_dest->val < op_src->val;
+	cpu.SF=result >> len;
+    	int s1,s2;
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
+    	cpu.ZF=!result;
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
+
+    // cpu.tmp = true;
+    // cf_sub(op_dest->val, op_src->val);
+    // DATA_TYPE res = op_dest->val - op_src->val;
+    // res ^= res >>4;
+	// res ^= res >>2;
+	// res ^= res >>1;
+	// cpu.PF=!(res & 1);
+    // // parity_flag(res);
+    // adjust_flag(op_dest->val, -op_src->val);
+    // cpu.ZF = !res;
+    // zero_flag(res);
+    // cpu.SF = sign_bit32(res);
+    // // sign_flag(res);
+    // overflow_flag(op_dest->val, -op_src->val);
     print_asm_template2();
 }
 
