a7b718e0cfaff639a89c2faf57ae75013428eaf1
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 00:15:36 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:15:36 up 1 day, 11:41,  2 users,  load average: 0.16, 0.13, 0.09 e60e8e88407a3417c15fd453e69d15f7e46bb4f

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -5,18 +5,6 @@
 static void do_execute () {
 	DATA_TYPE result = op_src->val + 1;
 	OPERAND_W(op_src, result);
-	/* TODO: Update EFLAGS. */
-	// panic("please implement me");
-
-	// int len = (DATA_BYTE << 3) - 1;
-	// cpu.OF=(result < op_dest->val);
-	// cpu.SF=result >> len;
-	// cpu.ZF=!result;
-	// OPERAND_W(op_src, result);
-	// result ^= result >>4;
-	// result ^= result >>2;
-	// result ^= result >>1;
-	// cpu.PF=!(result & 1);
 
 	uint32_t res = result;
     parity_flag(res);
