c2b62e0a99c085349b912bb9b4eaaddf061f73bb
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 01:03:52 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:03:52 up 1 day, 12:29,  2 users,  load average: 0.21, 0.16, 0.10 be0d232eac28db0feb7e09bcae1ea586fdc00d5

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -5,9 +5,11 @@
 static void do_execute(){
     op_src->val += cpu.CF;
     concat(write_operand_, SUFFIX)(op_dest, op_dest->val - op_src->val);
-    int res = carry_flag_sub(op_dest->val, op_src->val);
+    int res = op_dest->val - op_src->val;
+    cf_sub;
+    // int res = carry_flag_sub(op_dest->val, op_src->val);
     // int res = op_dest->val - op_src->val;
-    cpu.CF = op_dest->val < op_src->val;
+    // cpu.CF = op_dest->val < op_src->val;
     parity_flag(res);
     adjust_flag(op_dest->val, -op_src->val);
     zero_flag(res);
