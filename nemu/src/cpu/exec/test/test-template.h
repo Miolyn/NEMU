953c362ae229967fd30d1ffc58591a343d084876
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-16 02:22:53 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:22:53 up 1 day, 13:48,  2 users,  load average: 0.11, 0.10, 0.09 421174aa6af696422a2366dbaa74c75c0d193cd

--- nemu/src/cpu/exec/test/test-template.h
+++ nemu/src/cpu/exec/test/test-template.h
@@ -5,12 +5,23 @@
 static void do_execute(){
     printf("src:0x%x,dest:%x => 0x%x\n", op_src->val, op_dest->val, op_src->val & op_dest->val);
     
-    uint32_t res = op_src->val & op_dest->val;
-    cpu.OF = 0;
-    cpu.CF = 0;
-    sign_flag(res);
-    zero_flag(res);
-    parity_flag(res);
+	DATA_TYPE result = op_dest->val & op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF=0;
+	cpu.OF=0;
+	cpu.SF=result >> len;
+    	cpu.ZF=!result;
+    	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
+
+    // uint32_t res = op_src->val & op_dest->val;
+    // cpu.OF = 0;
+    // cpu.CF = 0;
+    // sign_flag(res);
+    // zero_flag(res);
+    // parity_flag(res);
     print_asm_template2();
 }
 
