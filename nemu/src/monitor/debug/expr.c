1fb61a4434dd37977f8da87f4ca865f7dcfe000a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-17 01:41:09 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:41:09 up 2 days, 38 min,  2 users,  load average: 0.13, 0.12, 0.09 2cb963c2c289dc7f225fb5cf6f3cd3ff1af9f1ad

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -125,12 +125,15 @@ static bool make_token(char *e) {
                     case LOR:
                     case HEXADECIMAL:
                     case REG:
+                    case VAR:
                     case NUMBER: {
                         tokens[nr_token].type = rules[i].token_type;
                         strncpy(tokens[nr_token].str, substr_start, substr_len);
                         tokens[nr_token++].str[substr_len] = '\0';
                         break;
                     }
+                    default: 
+                        assert(0);
 				}
 
 				break;
