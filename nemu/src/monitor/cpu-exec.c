d4fcfdc52d32566871f6a0cb749d7ca5b2cc7405
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 06:17:45 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:17:45 up 2 days, 15:08,  2 users,  load average: 0.25, 0.16, 0.11 3632678b64088a821d35168922a016e52e385409

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -104,14 +104,14 @@ void cpu_exec(volatile uint32_t n) {
 		}
 		cpu.eip += instr_len;
 
-// #ifdef DEBUG
+#ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
 		Log_write("%s\n", asm_buf);
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
-// #endif
+#endif
 
 		/* TODO: check watchpoints here. */
         check_wp(&nemu_state);
