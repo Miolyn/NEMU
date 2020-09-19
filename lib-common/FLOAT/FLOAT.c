4145e6d5c1889eeec7944bbe9282dce1cbb7906c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 19:40:25 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:40:25 up 2 days, 20:47,  2 users,  load average: 0.08, 0.10, 0.09 db97da977dfd502ce51c73976f1b3784d0aad6a7

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,61 +1,98 @@
 #include "FLOAT.h"
+// #include "lib-common/FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long c = (long long)a * (long long)b;
-	return (FLOAT)(c >> 16);
+
+	return (FLOAT)((ll)a * (ll)b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	int sign = 1;
-	if (a < 0) 
-	{
-		sign = -sign;
-		a = -a;
-	}
-	if (b < 0) 
-	{
-		sign = -sign;
-		b = -b;
-	}
-	int res = a / b;
-	a = a % b;
+	/* Dividing two 64-bit integers needs the support of another library
+	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
+	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
+	 * here. A "64/32" division is enough.
+	 *
+	 * To perform a "64/32" division, you can use the x86 instruction
+	 * `div' or `idiv' by inline assembly. We provide a template for you
+	 * to prevent you from uncessary details.
+	 *
+	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
+	 *
+	 * If you want to use the template above, you should fill the "???"
+	 * correctly. For more information, please read the i386 manual for
+	 * division instructions, and search the Internet about "inline assembly".
+	 * It is OK not to use the template above, but you should figure
+	 * out another way to perform the division.
+	 */
+	int sa = sign_bit(a);
+	int sb = sign_bit(b);
+	int s = sa * sb;
+	int noSa = a * int_no_sign(sa);
+	int noSb = b * int_no_sign(sb);
+	int res = noSa / noSb;
+	int mod = noSa % noSb;
 	int i;
-	for (i = 0; i < 16; i++) 
-	{
-		a <<= 1;
+	for(i = 0; i < 16; i++){
 		res <<= 1;
-		if (a >= b) 
-		{
-			a -= b;
-			res++;
+		mod <<= 1;
+		if(mod > noSb){
+			mod -= noSb;
+			res |= 1;
 		}
 	}
-	return res * sign;
+	return res * s;
 }
 
 FLOAT f2F(float a) {
-	int b = *(int *)&a;
-	int sign = b >> 31;
-	int exp = (b >> 23) & 0xff;
-	FLOAT k = b & 0x7fffff;
-	if (exp != 0) k += 1 << 23;
-	exp -= 150;
-	if (exp < -16) k >>= -16 - exp;
-	if (exp > -16) k <<= exp + 16;
-	return sign == 0 ? k : -k;
+	/* You should figure out how to convert `a' into FLOAT without
+	 * introducing x87 floating point instructions. Else you can
+	 * not run this code in NEMU before implementing x87 floating
+	 * point instructions, which is contrary to our expectation.
+	 *
+	 * Hint: The bit representation of `a' is already on the
+	 * stack. How do you retrieve it to another variable without
+	 * performing arithmetic operations on it directly?
+	 */
+	// int *addr = &a;
+	int t = *((int*)&a);
+	int s = sign_bit(t);
+	int noSa = (s << 1) >> 1;
+	int E = ((noSa >> 23)) & 0xff;
+	int m = noSa & 0x7ffff;
+	FLOAT res = m;
+	int e = E - 0x7f;
+	if(!E){
+		if(!m) return 0;
+		else e = 1 - E;
+	} else if(!(E ^0xff)){
+		return (-1) ^ (!s << 31);
+	}else res |= 0x80000;
+	// now point is at l:23
+	// (s)0 12345678¡¢9(10)(11)(12)(13)(14)(15).(16)
+	// now 
+	if(e >= 32 || e <= -32){
+		res = 0;
+	} else if(e > 0){
+		res <<= e;
+	} else{
+		e *= -1;
+		res >>= e;
+	}
+	res >>= 7;
+
+	return (FLOAT) (res * int_sign(s));
 }
 
 FLOAT Fabs(FLOAT a) {
-	FLOAT b;
-	if (a < 0)
-		b = - a;
-	else
-		b = a;
-	return b;
+	// nemu_assert(0);
+	return a * int_no_sign(sign_bit(a));
 }
+
+/* Functions below are already implemented */
 
 FLOAT sqrt(FLOAT x) {
 	FLOAT dt, t = int2F(2);
+
 	do {
 		dt = F_div_int((F_div_F(x, t) - t), 2);
 		t += dt;
