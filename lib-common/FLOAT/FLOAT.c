365908d07e788ecd7033de862fd3896ece6a7bd3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 19:49:41 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:49:41 up 2 days, 20:57,  2 users,  load average: 0.15, 0.13, 0.10 79cdc3c52249659aafd35baf8d3b87819b735f2

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,9 +1,13 @@
 #include "FLOAT.h"
 // #include "lib-common/FLOAT.h"
 
-FLOAT F_mul_F(FLOAT a, FLOAT b) {
+// FLOAT F_mul_F(FLOAT a, FLOAT b) {
 
-	return (FLOAT)((ll)a * (ll)b) >> 16;
+// 	return (FLOAT)((ll)a * (ll)b) >> 16;
+// }
+FLOAT F_mul_F(FLOAT a, FLOAT b) {
+	long long c = (long long)a * (long long)b;
+	return (FLOAT)(c >> 16);
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
