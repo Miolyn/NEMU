1a407e263f41dd0d87c31c36a71a9674c7576675
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 22:17:34 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:17:34 up 2 days, 23:24,  2 users,  load average: 0.06, 0.09, 0.09 5ef0c64e4f5b10d8c33bd0cf7a693b31a4c90b1

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -81,13 +81,17 @@ FLOAT f2F(float a) {
 	}else res |= (1 << 23);
 	// now point is at l:23
 	// (s)(31) (30)--(23).(22)--(16).(15)...(0)
+	// if(e > 7){
+	// 	res <<= e - 7;
+	// } else{
+	// 	e *= -1;
+	// 	res >>= e - 7;
+	// }
 	if(e > 7){
 		res <<= e - 7;
 	} else{
-		if(e > 0)
-			res >>= 7 + e;
-		else
-			res >>= 7 - e;
+		if(e > 0) res >>= 7 - e;
+		else res >>= 7 - e;
 	}
 	// res >>= 7;
 
