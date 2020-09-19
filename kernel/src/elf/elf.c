c166c53b9d7a7eb56b9846181899f640fb78a9d4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 17:32:55 -0700

    >  compile kernel 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:32:55 up 2 days, 18:40,  2 users,  load average: 0.30, 0.17, 0.11 45c95447d0ba5b0e37a158e4c40bc72040f5b4f1

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -41,7 +41,7 @@ uint32_t loader() {
 	int i;
 	for(i = 0; i < elf->e_phnum; i++) {
 		/* Scan the program header table, load each segment into memory */
-		ph = (void*)(elf + elf->e_phoff + i * elf->e_phentsize);
+		ph = (void*)(buf + elf->e_phoff + i * elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
 			
 			/* TODO: read the content of the segment from the ELF file 
