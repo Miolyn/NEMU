23d3552affd81e1336f59a17598089b22aac49d1
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 07:15:18 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:15:18 up 4 days, 19:32,  3 users,  load average: 0.16, 0.12, 0.09 dc5651e162b1ce4b4f3c04a6d292d9f78b09d9b2

--- nemu/include/cpu/cache.h
+++ nemu/include/cpu/cache.h
@@ -4,7 +4,7 @@
 #include "common.h"
 
 // cache block 64Byte cache:64KB
-// 8way 
+// 8way
 // 2^3 way * 2^7* 64Byte
 #define SET_WIDTH 3
 #define LINE_WIDTH 7
@@ -14,13 +14,32 @@
 #define CACHE_SET (1 << SET_WIDTH)
 #define CACHE_LINE (1 << LINE_WIDTH)
 #define CACHE_BLOCK (1 << BLOCK_WIDTH)
-typedef struct{
-    uint32_t valid :1;
-    uint32_t tag :TAG_WIDTH;
+
+// 2^3 * 2^7 * 2^6
+uint8_t _cache[CACHE_SET][CACHE_LINE][CACHE_BLOCK];
+uint8_t *hw_cache = (void*)_cache;
+typedef struct {
+    uint32_t valid : 1;
+    uint32_t tag : TAG_WIDTH;
     uint8_t block[CACHE_BLOCK];
-    uint32_t dirt_bit :1;
-}Cacheline;
+    uint32_t dirt_bit : 1;
+} CacheLine;
 
+typedef struct {
+    CacheLine cacheLine[CACHE_LINE];
+    uint32_t setID;
+} CacheSet;
 
+#pragma pack(1)
+typedef struct {
+    uint32_t tag : TAG_WIDTH;
+    uint32_t set : SET_WIDTH;
+    uint32_t blockOffset : BLOCK_WIDTH;
+} cacheAddr;
+#pragma pack()
+
+typedef struct {
+    CacheSet cacheSet[CACHE_SET];
+}Cache;
 
 #endif
