6b8e4257bb67cf1720d477119459b82410c27389
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 20:31:41 -0700

    >  run 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:31:41 up 4 days, 14:11,  4 users,  load average: 0.16, 0.15, 0.10 9b92024e35af23164299734c49bc14b1b761bbd

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -15,9 +15,11 @@ int tf = 0;
 int tfz = 0;
 int tcnt = 0;
 int powTen(int n){
+	if(n <= 1){printf("\n");}
     int res = 1;
-    while(n-- != 0){
+    while(n != 0){
         res = res * 10;
+		n -= 1;
     }
     return res;
 }
