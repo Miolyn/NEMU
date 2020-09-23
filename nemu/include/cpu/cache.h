1718624be12b3e8981adda2a929c81d9e9318a7e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 07:49:58 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:49:58 up 4 days, 20:07,  2 users,  load average: 0.20, 0.14, 0.10 8a4ac33ed0ff12413556868f51994c475a3f2f2a

--- nemu/include/cpu/cache.h
+++ nemu/include/cpu/cache.h
@@ -6,40 +6,6 @@
 // cache block 64Byte cache:64KB
 // 8way
 // 2^3 way * 2^7* 64Byte
-#define SET_WIDTH 3
-#define LINE_WIDTH 7
-#define BLOCK_WIDTH 6
-#define TAG_WIDTH (32 - SET_WIDTH - LINE_WIDTH - BLOCK_WIDTH)
-
-#define CACHE_SET (1 << SET_WIDTH)
-#define CACHE_LINE (1 << LINE_WIDTH)
-#define CACHE_BLOCK (1 << BLOCK_WIDTH)
-
-// 2^3 * 2^7 * 2^6
-uint8_t _cache[CACHE_SET][CACHE_LINE][CACHE_BLOCK];
-uint8_t *hw_cache = (void*)_cache;
-typedef struct {
-    uint32_t valid : 1;
-    uint32_t tag : TAG_WIDTH;
-    uint8_t block[CACHE_BLOCK];
-    uint32_t dirt_bit : 1;
-} CacheLine;
-
-typedef struct {
-    CacheLine cacheLine[CACHE_LINE];
-    uint32_t setID;
-} CacheSet;
-
-#pragma pack(1)
-typedef struct {
-    uint32_t tag : TAG_WIDTH;
-    uint32_t set : SET_WIDTH;
-    uint32_t blockOffset : BLOCK_WIDTH;
-} cacheAddr;
-#pragma pack()
-
-typedef struct {
-    CacheSet cacheSet[CACHE_SET];
-}Cache;
+extern int8_t *hw_cache;
 
 #endif
---
+++ nemu/src/cpu/cache/cache.c
@@ -0,0 +1,37 @@
+#include "common.h"
+// 2^3 * 2^7 * 2^6
+// uint8_t _cache[CACHE_SET][CACHE_LINE][CACHE_BLOCK];
+// uint8_t *hw_cache = (void*)_cache;
+#define SET_WIDTH 3
+#define LINE_WIDTH 7
+#define BLOCK_WIDTH 6
+#define TAG_WIDTH (32 - SET_WIDTH - LINE_WIDTH - BLOCK_WIDTH)
+
+#define CACHE_SET (1 << SET_WIDTH)
+#define CACHE_LINE (1 << LINE_WIDTH)
+#define CACHE_BLOCK (1 << BLOCK_WIDTH)
+uint8_t _cache[CACHE_SET][CACHE_LINE][CACHE_BLOCK];
+uint8_t *hw_cache = (void*)_cache;
+typedef struct {
+    uint32_t valid : 1;
+    uint32_t tag : TAG_WIDTH;
+    uint8_t block[CACHE_BLOCK];
+    uint32_t dirt_bit : 1;
+} CacheLine;
+
+typedef struct {
+    CacheLine cacheLine[CACHE_LINE];
+    uint32_t setID;
+} CacheSet;
+
+#pragma pack(1)
+typedef struct {
+    uint32_t tag : TAG_WIDTH;
+    uint32_t set : SET_WIDTH;
+    uint32_t blockOffset : BLOCK_WIDTH;
+} cacheAddr;
+#pragma pack()
+
+typedef struct {
+    CacheSet cacheSet[CACHE_SET];
+}Cache;
