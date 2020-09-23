3a876f13451fcd607f0baa2e29076f59bd208a57
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 07:01:43 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:01:42 up 4 days, 19:19,  3 users,  load average: 0.26, 0.16, 0.10 8d1d31836ccf9cdaed0e514c131867476937e0d

--- nemu/include/cpu/cache.h
+++ nemu/include/cpu/cache.h
@@ -3,9 +3,23 @@
 
 #include "common.h"
 
+// cache block 64Byte cache:64KB
+// 8way 
+// 2^3 way * 2^7* 64Byte
+#define SET_WIDTH 3
+#define LINE_WIDTH 7
+#define BLOCK_WIDTH 6
+#define TAG_WIDTH (32 - SET_WIDTH - LINE_WIDTH - BLOCK_WIDTH)
+
+#define CACHE_SET (1 << SET_WIDTH)
+#define CACHE_LINE (1 << LINE_WIDTH)
+#define CACHE_BLOCK (1 << BLOCK_WIDTH)
 typedef struct{
-    
-}CacheBlock;
+    uint32_t valid :1;
+    uint32_t tag :TAG_WIDTH;
+    uint8_t block[CACHE_BLOCK];
+    uint32_t dirt_bit :1;
+}Cacheline;
 
 
 
