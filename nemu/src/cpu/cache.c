28e6d82faafa2a17e86cc0760d0fb10b3338441f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-23 17:39:13 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:39:13 up 4 days, 22:16,  2 users,  load average: 0.13, 0.11, 0.09 63e87921949eefa5c8eef24072855724ccb7fbde

--- nemu/src/cpu/cache.c
+++ nemu/src/cpu/cache.c
@@ -21,7 +21,9 @@
 #define CACHE_BLOCK_l2 (1 << BLOCK_WIDTH_l2)
 
 uint8_t _cache_l1[CACHE_SET_l1][CACHE_LINE_l1][CACHE_BLOCK_l1];
-
+uint8_t _cache_l2[CACHE_SET_l2][CACHE_LINE_l2][CACHE_BLOCK_l2];
+uint8_t *hw_cache_l1 = (void*)_cache_l1;
+uint8_t *hw_cache_l2 = (void*)_cache_l2;
 void init_cache(){
     
 }
