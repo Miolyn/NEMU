fb349b940666539d6ec16e605622d6ecd84cea44
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 09:25:33 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:25:33 up 6 days, 13:58,  2 users,  load average: 0.24, 0.14, 0.10 b8b136b772bb37034f5cbb7bd433007b8cf0acda

--- nemu/src/cpu/exec/jmp/jmp-template.h
+++ nemu/src/cpu/exec/jmp/jmp-template.h
@@ -44,10 +44,11 @@ make_helper(concat(ljmp_ptr_, SUFFIX)){
         pt1 = instr_fetch(eip + 4, 2);
         len = 7;
     }
+    printf("pt0:0x%x, pt1:0x%x\n", pt0, pt1);
     cpu.eip = pt0;
     cpu.sRegs[R_CS].selector.val = pt1;
     load_descriptor(R_CS);
-    print_asm("ljmp" " %u, %u", pt0, pt1);
+    print_asm("ljmp" " %x, %x", pt0, pt1);
     return len;
 }
 #endif
