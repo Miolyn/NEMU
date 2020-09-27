cd93af2f3dc7490059c3859c40dbc1e111f7fb2c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 08:42:57 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:42:57 up 7 days,  3:05,  4 users,  load average: 0.20, 0.11, 0.10 592558f03096da4cb87d8a578fdcd659d402761d

--- nemu/src/cpu/exec/data-mov/movsreg.c
+++ nemu/src/cpu/exec/data-mov/movsreg.c
@@ -3,7 +3,7 @@
 make_helper(mov_sreg2rm_w){
     decode_r2rm_w(eip + 1);
     reg_w(op_dest->reg) = cpu.sRegs[op_src->sreg].selector.val;
-    print_asm("movsreg " "cr%d", op_src->sreg);
+    print_asm("movsreg " "sreg%d", op_src->sreg);
     return 2;
 }
 
@@ -13,8 +13,8 @@ make_helper(mov_rm2sreg_w){
     // printf("op_src %d\n", op_src->reg);
     // printf("sreg %d\n", op_dest->sreg);
     cpu.sRegs[op_dest->sreg].selector.val = reg_w(op_src->reg);
-    if(op_dest->sreg == R_ES) printf("hello\n");
+    if(op_dest->sreg == R_DS) printf("hello\n");
     load_descriptor(op_dest->sreg);
-    print_asm("movsreg " "cr%d", op_dest->sreg);
+    print_asm("movsreg " "sreg%d", op_dest->sreg);
     return 2;
 }
