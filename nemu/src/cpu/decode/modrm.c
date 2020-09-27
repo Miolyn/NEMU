8de89a5bafbc588a41a99325f6ab45232814aa09
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 08:38:25 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:38:25 up 7 days,  3:00,  4 users,  load average: 0.11, 0.10, 0.09 323ea878e134fa20d35110d212ba206b2adb6397

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -104,11 +104,7 @@ int read_ModR_M(swaddr_t eip, Operand *rm, Operand *reg) {
 	if(m.mod == 3) {
 		rm->type = OP_TYPE_REG;
 		rm->reg = m.R_M;
-		// if(rm->reg == R_ESP || rm->reg == R_EBP){
-		// 	reg->sreg = R_SS;
-		// } else{
-		// 	reg->sreg = R_DS;
-		// }
+		if(rm->reg == R_ES) printf("hello\n");
 		reg->sreg = m.reg;
 		switch(rm->size) {
 			case 1: rm->val = reg_b(m.R_M); break;
