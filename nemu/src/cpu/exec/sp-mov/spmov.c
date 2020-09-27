8728503ab480f0a4d618cab2f9c07e5bc2301f41
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 07:05:05 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:05:05 up 7 days,  1:27,  4 users,  load average: 0.16, 0.14, 0.10 fd6c11bc81288fbf209909ddd54d4c747d7ee97f

--- nemu/src/cpu/exec/sp-mov/spmov.c
+++ nemu/src/cpu/exec/sp-mov/spmov.c
@@ -3,11 +3,11 @@
 make_helper(mov_cr2r_v){
     // assert(cpu.cr0.protect_enable == 0);
     decode_r2rm_l(eip + 1);
-    if(op_src->val == 0){
+    if(op_src->reg == 0){
         printf("mov cr0 2 %d\n", op_dest->reg);
         reg_l(op_dest->reg) = cpu.cr0.val;
         printf("cr0 pe:%d\n", cpu.cr0.protect_enable);
-    } else if(op_src->val == 3){
+    } else if(op_src->reg == 3){
         reg_l(op_dest->reg) = cpu.cr3.val;
     }
     return 2;
@@ -17,12 +17,11 @@ make_helper(mov_cr2r_v){
 make_helper(mov_r2cr_v){
     // assert(cpu.cr0.protect_enable == 0);
     decode_rm2r_l(eip + 1);
-    printf("op_dest:%d\n", op_dest->val);
-    if(op_dest->val == 0){
+    if(op_dest->reg == 0){
         printf("mov %d 2 cr0\n", op_dest->reg);
         cpu.cr0.val = reg_l(op_src->reg);
         printf("cr0 pe:%d\n", cpu.cr0.protect_enable);
-    } else if(op_dest->val == 3){
+    } else if(op_dest->reg == 3){
         cpu.cr3.val = reg_l(op_src->reg);
     }
     return 2;
