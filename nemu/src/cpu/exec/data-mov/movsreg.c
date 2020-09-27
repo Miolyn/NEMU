647a30e986a87643631aafb555ff1dc5a4185c4e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 16:58:55 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:58:55 up 7 days,  4:31,  4 users,  load average: 0.19, 0.14, 0.10 ae7d01d53af722a07c8fb2281a84aa06c08f908d

--- nemu/src/cpu/exec/data-mov/movsreg.c
+++ nemu/src/cpu/exec/data-mov/movsreg.c
@@ -4,7 +4,7 @@ make_helper(mov_sreg2rm_w){
     decode_r2rm_w(eip + 1);
     printf("op_dest->reg:%d\n", op_dest->reg);
     // reg_w(op_dest->reg) = cpu.sRegs[op_src->sreg].selector.val;
-    reg_w(R_EAX) = cpu.sRegs[op_src->sreg].selector.val;
+    reg_l(R_EAX) = cpu.sRegs[op_src->sreg].selector.val;
     print_asm("movsreg " "sreg%d", op_src->sreg);
     return 2;
 }
@@ -13,7 +13,7 @@ make_helper(mov_rm2sreg_w){
     decode_rm2r_w(eip + 1);
     printf("sreg:%d\n", op_dest->sreg);
     // cpu.sRegs[op_dest->sreg].selector.val = reg_w(op_src->reg);
-    cpu.sRegs[op_dest->sreg].selector.val = reg_w(R_EAX);
+    cpu.sRegs[op_dest->sreg].selector.val = reg_l(R_EAX);
     load_descriptor(op_dest->sreg);
     print_asm("movsreg " "sreg%d", op_dest->sreg);
     return 2;
