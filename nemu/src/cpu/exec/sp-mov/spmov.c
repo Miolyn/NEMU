d52cddcc89ff582cf9a2f6d6acd3df8fc0dc8438
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 20:37:49 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:37:49 up 7 days,  6:22,  3 users,  load average: 0.13, 0.11, 0.10 56f94a6bb779b7b951bb626d8da4d1496775c35

--- nemu/src/cpu/exec/sp-mov/spmov.c
+++ nemu/src/cpu/exec/sp-mov/spmov.c
@@ -5,10 +5,12 @@ make_helper(mov_cr2r_v){
     decode_r2rm_l(eip + 1);
     if(op_src->reg == 0){
         printf("mov cr0 2 %d\n", op_dest->reg);
-        reg_l(op_dest->reg) = cpu.cr0.val;
+        // reg_l(op_dest->reg) = cpu.cr0.val;
+        reg_l(R_EAX) = cpu.cr0.val;
         printf("cr0 pe:%d, pagin:%d\n", cpu.cr0.protect_enable, cpu.cr0.paging);
     } else if(op_src->reg == 3){
-        reg_l(op_dest->reg) = cpu.cr3.val;
+        // reg_l(op_dest->reg) = cpu.cr3.val;
+        reg_l(R_EAX) = cpu.cr3.val;
     }
     print_asm("mov cr%d -> reg%d", op_src->reg, op_dest->reg);
     return 2;
@@ -20,10 +22,12 @@ make_helper(mov_r2cr_v){
     decode_rm2r_l(eip + 1);
     if(op_dest->reg == 0){
         printf("mov %d 2 cr0\n", op_dest->reg);
-        cpu.cr0.val = reg_l(op_src->reg);
+        // cpu.cr0.val = reg_l(op_src->reg);
+        cpu.cr0.val = reg_l(R_EAX);
         printf("cr0 pe:%d, pagin:%d\n", cpu.cr0.protect_enable, cpu.cr0.paging);
     } else if(op_dest->reg == 3){
-        cpu.cr3.val = reg_l(op_src->reg);
+        // cpu.cr3.val = reg_l(op_src->reg);
+        cpu.cr3.val = reg_l(R_EAX);
         printf("page base addr%x\n", cpu.cr3.page_directory_base);
     }
     print_asm("mov reg%d -> cr%d", op_dest->reg, op_src->reg);
