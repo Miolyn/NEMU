660c2028de358def8d0cf29c36aead28a587535b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 20:34:30 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:34:30 up 7 days,  6:19,  3 users,  load average: 0.09, 0.11, 0.11 44b6d488f60c05b6665e4dee6ff427f48c14d7ff

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -204,6 +204,10 @@ uint32_t page_translate(lnaddr_t addr, uint32_t len){
 	lnAddr.val = addr;
 	printf("addr:0x%x\n", addr);
 	printf("dir:%x,page:%x,off:%x\n", lnAddr.dir, lnAddr.page, lnAddr.offset);
+	int i;
+	for(i = 0; i <= lnAddr.dir; i++){
+		printf("%x\n", hwaddr_read(FRAME_ADDR(dirBaseAddr) + i * 4, 4));
+	}
 	uint32_t dirPageEntryVal = hwaddr_read(FRAME_ADDR(dirBaseAddr) + lnAddr.dir * 4, 4);
 	dirPageEntry.val = dirPageEntryVal;
 	assert(dirPageEntry.p);
