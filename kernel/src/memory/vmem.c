8985118d42e97bc67c71b8e5afe288a856f336f8
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-05 06:19:40 -0700

    >  compile kernel 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:19:40 up 8 days, 10:38,  1 user,  load average: 0.28, 0.18, 0.14 1d3668d0f59f631ed5bf30a5e8d8de038736123e

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -1,43 +1,32 @@
 #include "common.h"
 #include "memory.h"
 #include <string.h>
-
+#include "trap.h"
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
 #define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE)
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
-PTE vmem_pt[1024] align_to_page;
+PTE my_pt[1][NR_PTE] align_to_page;
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	PDE *pdp = (PDE *)va_to_pa(get_updir());
-//	pdp[VMEM_ADDR/PT_SIZE].val=0;
-	pdp[VMEM_ADDR/PT_SIZE].page_frame=(uint32_t)va_to_pa(vmem_pt)>>12;
-	pdp[VMEM_ADDR/PT_SIZE].present=1;
-	int index = VMEM_ADDR/PAGE_SIZE;
-	for(;index<(VMEM_ADDR+SCR_SIZE)/PAGE_SIZE+1;index++){
-		//Log("addr is %x",vmem_pt+index);
-//		vmem_pt[index].val=0;
-		vmem_pt[index].page_frame=index+(NR_PT-1)*NR_PTE;
-		//Log("%x.val is %x\n",index,vmem_pt[index].page_frame);
-		vmem_pt[index].present=1;
+
+
+set_bp();
+	PDE *pdir = get_updir();
+	PTE *pt = my_pt[0];
+	pdir[0].val = make_pde(va_to_pa(pt));
+	int total = SCR_SIZE / PAGE_SIZE + 1;
+	uint32_t addr = VMEM_ADDR;
+	int i = 0;
+	for(; i < total; i++){
+		pt[(addr >> 12) & 0x3ff].val = make_pte(addr);
+		addr += PAGE_SIZE;
 	}
-
-
-	// PDE *pdir = get_updir();
-	// PTE *pt = my_pt[0];
-	// pdir[0].val = make_pde(va_to_pa(pt));
-	// int total = SCR_SIZE / PAGE_SIZE + 1;
-	// uint32_t addr = VMEM_ADDR;
-	// int i = 0;
-	// for(; i < total; i++){
-	// 	pt[(addr >> 12) & 0x3ff].val = make_pte(addr);
-	// 	addr += PAGE_SIZE;
-	// }
 	// PTE *ptable = getPTE();
 	// int i = VMEM_ADDR / PAGE_SIZE;
 	// void *pframe_addr = ((void*)VMEM_ADDR);
