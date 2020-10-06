43db6e877362e9fb9c95d6a493c3240dab34608e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-05 18:43:58 -0700

    >  compile kernel 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:43:58 up 8 days, 14:21,  1 user,  load average: 0.17, 0.19, 0.19 5ee8f9e2d41f98cb639833fad88890e69db278c2

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -44,7 +44,12 @@ void init_page(void) {
 	 */
 
 
-
+	asm volatile ("std;\
+	 1: stosl;\
+		subl %0, %%eax;\
+		jge 1b;\
+		cld" : :
+		"i"(PAGE_SIZE), "a"((PHY_MEM - PAGE_SIZE) | 0x7), "D"(ptable - 1));
 
 	/*
 		===== referenced code for the inline assembly above =====
