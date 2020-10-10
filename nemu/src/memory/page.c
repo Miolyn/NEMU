27cad5aa68431437cdc0d516a36dc12b139dc3d9
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-10 07:27:39 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:27:39 up 8 days, 19:08,  1 user,  load average: 0.07, 0.09, 0.09 2dd72d603729914ce796baf0f500d25ce2918486

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -9,7 +9,7 @@ uint32_t page_translate(lnaddr_t addr, uint32_t len){
 	PageTableEntry pageEntry;
 	LinearAddr lnAddr;
 	lnAddr.val = addr;
-	bool suc = false;
+	// bool suc = false;
 
 	if(addr == 0x7ffffa4){
 		printf("dirbase:%d\n", cpu.cr3.page_directory_base);
@@ -17,8 +17,8 @@ uint32_t page_translate(lnaddr_t addr, uint32_t len){
 		printf("eip at 0x7ffffa4 is 0x%x\n", cpu.eip);
 		// assert(0);
 	}
-	uint32_t tlb_res = read_tlb(addr, &suc);
-	if(suc) return tlb_res + lnAddr.offset;
+	// uint32_t tlb_res = read_tlb(addr, &suc);
+	// if(suc) return tlb_res + lnAddr.offset;
 	uint32_t dirBaseAddr = cpu.cr3.page_directory_base;
 
 	uint32_t dirPageEntryVal = hwaddr_read(FRAME_ADDR(dirBaseAddr) + lnAddr.dir * 4, 4);
@@ -32,7 +32,7 @@ uint32_t page_translate(lnaddr_t addr, uint32_t len){
 	if(lnAddr.offset + len <= 1 << 12){
 		// res = hwaddr_read(FRAME_ADDR(pageEntry.pageFrameAddr) + lnAddr.offset, len);
 		res = FRAME_ADDR(pageEntry.pageFrameAddr) + lnAddr.offset;
-		write_tlb(addr, FRAME_ADDR(pageEntry.pageFrameAddr));
+		// write_tlb(addr, FRAME_ADDR(pageEntry.pageFrameAddr));
 		// printf("page addr:0x%x\n", res);
 	} else{
 		assert(0);
