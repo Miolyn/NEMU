3de6bc3da70c94673383f56ee23997b7f54edc81
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-10 18:51:44 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:51:44 up 9 days,  4:21,  1 user,  load average: 0.09, 0.12, 0.09 77eb56043fb6d76592d022eaa8d23e48d5a6519f

--- nemu/src/cpu/exec/lgdt/lgdt-template.h
+++ nemu/src/cpu/exec/lgdt/lgdt-template.h
@@ -5,7 +5,8 @@
 #if DATA_BYTE != 1
 make_helper(concat(lgdt_m_, SUFFIX)){
     int len = concat3(decode_rm_, SUFFIX, _internal)(eip + 1, op_src, op_src2);
-    uint16_t limit = lnaddr_read(op_src->addr, 2);
+    // uint16_t limit = lnaddr_read(op_src->addr, 2);
+    uint16_t limit = swaddr_read(op_src->addr, 2, R_DS);
     uint32_t base = 0;
     if(ops_decoded.is_operand_size_16){
         base = lnaddr_read(op_src->addr + 2, 3);
