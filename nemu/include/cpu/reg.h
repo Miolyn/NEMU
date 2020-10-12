ca81537b16d7d3abc05e59924cc5147c760852ce
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-11 20:02:51 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:02:51 up 9 days, 18:50,  1 user,  load average: 0.25, 0.18, 0.12 5f0ac227182a443669c8ec769b4f12cf1702248c

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -181,7 +181,31 @@ typedef struct {
 	TLBLine *TLBLines;
 	int lineNum;
 } TLBCache;
+
 extern TLBCache TLB;
+
+typedef struct{
+	union {
+		struct{
+			uint32_t offset0 : 16;
+			uint32_t selector : 16;
+		};
+		uint32_t val1;
+	};
+
+	union{
+		struct {
+			uint32_t : 5;
+			uint32_t zero : 3;
+			uint32_t zero1 : 5;
+			uint32_t dpl : 2;
+			uint32_t p : 1;
+		};
+		uint32_t val2;
+	};
+	
+} InteruptGate;
+
 static inline int check_reg_index(int index) {
 	assert(index >= 0 && index < 8);
 	return index;
