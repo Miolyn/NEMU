657d706ec12924d829fc9ea2507fdcdf0570b406
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-13 05:03:21 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:03:21 up 10 days, 16:12,  1 user,  load average: 0.42, 0.28, 0.20 2dc052b2f9ca83a0d10fb126e19b3cc97bf2c1d7

--- nemu/src/cpu/exec/io/out-template.h
+++ nemu/src/cpu/exec/io/out-template.h
@@ -3,9 +3,9 @@
 #define instr out
 
 make_helper(concat(out_a2i_, SUFFIX)){
-    ioaddr_t ioNo = instr_fetch(eip + 1, 1);
+    ioaddr_t ioNo = instr_fetch(eip + 1, DATA_BYTE);
     pio_write(ioNo, DATA_BYTE, REG(R_EAX));
-    return 2;
+    return 1 + DATA_BYTE;
 }
 
 make_helper(concat(out_a2dx_,SUFFIX)){
