079e29d01aa9c34aaad7b656378d87d206eb8556
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-12 21:25:48 -0700

    >  compile kernel 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:25:48 up 10 days,  9:35,  1 user,  load average: 0.10, 0.14, 0.10 e98121cb902df48b9865bc9f7779c7e542d01f2e

--- kernel/include/irq.h
+++ kernel/include/irq.h
@@ -14,8 +14,9 @@
 // } TrapFrame;
 
 typedef struct TrapFrame {
-	uint32_t eax, ecx, edx, ebx, old_esp, ebp, esi, edi, eflags, cs, eip, error_code;
+	uint32_t edi, esi, ebp, old_esp, ebx, edx, ecx, eax;
 	int32_t irq;
+	uint32_t error_code, eip, cs, eflags;
 } TrapFrame;
 
 #endif
