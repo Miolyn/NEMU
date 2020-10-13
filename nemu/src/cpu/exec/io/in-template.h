8fe277790e1d0ecb1fcecd3b174e2555a625e9ed
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-13 05:04:16 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-47-generic #51~18.04.1-Ubuntu SMP Sat Sep 5 14:35:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:04:16 up 10 days, 16:13,  1 user,  load average: 0.50, 0.32, 0.21 120b55b3e9fa4ae6600960768d030004b0e8e963

--- nemu/src/cpu/exec/io/in-template.h
+++ nemu/src/cpu/exec/io/in-template.h
@@ -3,10 +3,10 @@
 #define instr in
 
 make_helper(concat(in_i2a_, SUFFIX)){
-    
-    ioaddr_t ioNo = instr_fetch(eip + 1, DATA_BYTE);
+    assert(0);
+    ioaddr_t ioNo = instr_fetch(eip + 1, 1);
     REG(R_EAX) = pio_read(ioNo, DATA_BYTE);
-    return 1 + DATA_BYTE;
+    return 2;
 }
 
 make_helper(concat(in_dx2a_,SUFFIX)){
