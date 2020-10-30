74fd59b88aea793261c97231b2e9a2435e356084
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 01:07:01 -0700

    >  compile NEMU 3018216166 liyining Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:07:01 up 10:45,  1 user,  load average: 1.02, 1.22, 0.85 fcd3c72746b8b7eb25dc8055b71d4639479b9bce

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -99,7 +99,7 @@ void cpu_exec(volatile uint32_t n) {
 		swaddr_t eip_temp = cpu.eip;
 		if((n & 0xffff) == 0) {
 			/* Output some dots while executing the program. */
-			fputc('.', stderr);
+			// fputc('.', stderr);
 		}
 #endif
 
